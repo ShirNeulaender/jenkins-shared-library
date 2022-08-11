b1510e9b71640b4a024ec56cf3c27337197042ed
ShirNeulaender <110778882+ShirNeulaender@users.noreply.github.com>
2022-08-11 20:51:39 +0300

    init commit

---
+++ 1.py
@@ -0,0 +1,26 @@
+a = 1 + 1
+b = "hello" + "world"
+c = a * "Aviel"
+d = 12 % 5
+print(a)
+print(b)
+print(c)
+print(d)
+e = "Hello"
+f = "World"
+g = f"{e} {f}"  # the favorite
+print(g)
+h = "%s %s" % (e, f)
+print(h)
+# חיבור מחרוזות
+i = "{} {}".format(e, f)
+print(i)
+j = "Hello"
+k = 'Moshe'
+print(j)
+print(k)
+m = 'shir \n\"rocks\"'
+print(m)
+# \n row below, \t tab , \r back to the beginning of the row
+
+
